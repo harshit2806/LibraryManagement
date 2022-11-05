0d44f48c99b4d0ca602e88e92abc6a135668e3b3
Harshit <harshitsoni2806@gmail.com>
2022-11-05 12:42:19 +0530

    .

--- src/main/java/com/saini/lib/model/Book.java
+++ src/main/java/com/saini/lib/model/Book.java
@@ -1,3 +1,5 @@
+package com.saini.lib.model;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -9,5 +11,28 @@ import javax.persistence.Id;
 		private String  authorName;
 		private Integer noOfBooks;
 		
-		
+		public Integer getBookIdNo() {
+			return bookIdNo;
+		}
+		public void setBookIdNo(Integer bookIdNo) {
+			this.bookIdNo = bookIdNo;
+		}
+		public String getBookName() {
+			return bookName;
+		}
+		public void setBookName(String bookName) {
+			this.bookName = bookName;
+		}
+		public String getAuthorName() {
+			return authorName;
+		}
+		public void setAuthorName(String authorName) {
+			this.authorName = authorName;
+		}
+		public Integer getNoOfBooks() {
+			return noOfBooks;
+		}
+		public void setNoOfBooks(Integer noOfBooks) {
+			this.noOfBooks = noOfBooks;
+		}
 }
